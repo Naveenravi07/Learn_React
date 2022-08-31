62731c00fa2e9fe1372a15a16a461d1d7c49bdea
Naveen Ravi <gamerkidnave@gmail.com>
2022-08-31 05:51:35 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
 import './App.css';
-import { useState } from 'react'
-import Navbar from './components/Navbar';
-import Counter from './components/counter';
-import AdressGen from './components/AdressGen';
-import Users from './components/Users';
-import Toogle from './components/toogle';
+import Profile from './components/Profile';
+import About from './components/About';
+import { useHistory, Route, Link } from 'react-router-dom'
 function App() {
-  let [state, setState] = useState(false)
-  let username = "shastri"
-  let count = 1
-  let users = [
-    {
-      name: 'Naveen',
-      age: '22',
-      place: 'Moon'
-    },
-    {
-      name: 'Shastri',
-      age: '15',
-      place: 'Sun'
-    },
-    {
-      name: 'Ezzzz',
-      age: '80',
-      place: 'Earth'
-    }
-  ]
+  const history = useHistory()
   return (
     <div className="App">
+      <button onClick={() => history.push('/about')}>About Me</button>
+      <button onClick={() => history.push('/profile')}>profile</button>
+      <Route path='/about' component={About} />
+      <Route path='/profile' component={Profile} />
 
-      <Navbar data={username} />
-      <h2>Learn React </h2>
-
-      <Day3 />
-      {
-        users.map((obj, index) => {
-          return (
-            <Users key={index} name={obj.name} place={obj.place} age={obj.age} />
-          )
-        })
-      },
-
-      <h2 onClick={() => {
-        setState(!state)
-      }}>Open / Close Counter  </h2>
-
-      {state && <Toogle />}
     </div>
   );
 }
 
 export default App;
-
-function Day3() {
-  return (
-    <h3 className='day01'>DAY 03 : Spread and UseEffect</h3>
-  )
-}
